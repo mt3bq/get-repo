1650f0941e2036d06b4dc8472b1aab9603b5a143
mt3bq <32024044+mt3bq@users.noreply.github.com>
2023-04-26 14:53:17 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+let name=["muteb","kiled","All","Momed","Nider"];
+
+
+
+let repo=document.querySelector('.box_repo');
+
+
+let info=document.createElement('div');
+
+info.className='info';
+
+info.childNodes('a');
+
+repo.appendChild(info);
+
+
+name.map((d)=>{
+
+
+})
